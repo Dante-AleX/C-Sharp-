01a8748d61f2110b9ee9fd21b4f411ad1b1cd4c3
Alex <alex.kojokari42@gmail.com>
2022-07-07 18:59:19 +0300

    add Example005 If

---
+++ Example005_If/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Please, insert your name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "mary")
+{   
+    Console.WriteLine("Hey, it's MARY!!");
+}
+else
+{
+    Console.WriteLine("Hello, ");
+    Console.WriteLine(username);
+}
